45414ff665027fd84b1b3e18265c84e4ad3d9406
Vishal2519 <83157402+Vishal2519@users.noreply.github.com>
2021-07-16 21:45:20 +0530

    Add files via upload

---
+++ Popup/content.js
@@ -0,0 +1,30 @@
+
+window.SpeechRecognition = window.SpeechRecognition
+|| window.webkitSpeechRecognition;
+
+
+
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+//recognition.maxAlternatives = 10;
+
+recognition.onresult = (e) => {
+for (var i = e.resultIndex; i < e.results.length; ++i) {
+    if (e.results[i].isFinal) {
+          chrome.runtime.sendMessage({type:"body",data: e.results[i][0].transcript});
+    }
+}
+}
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.data == "start"){
+    console.log("Message recieved");
+      recognition.start()
+    }
+    else if(request.data == "check"){
+      speechSynthesis.speak(new SpeechSynthesisUtterance("Please check your connection"));
+    }
+  });
